17a4b7cfded624bd5885fab84db45e9563f3ab87
khavkivar <sebastian.calderon@sansano.usm.cl>
2026-01-22 19:44:02 -0300

    feat: create a simple card

--- app/src/main/java/com/vasvani/orderpilot/MainActivity.kt
+++ app/src/main/java/com/vasvani/orderpilot/MainActivity.kt
@@ -4,26 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vasvani.orderpilot.ui.theme.OrderpilotTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val cardItems = listOf("Card 1", "Card 2", "Card 3")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             OrderpilotTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { MyTopBar() } 
+                ) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        cardItems.forEach { item ->
+                            Card(
+                                name = item,
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +57,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Card(name: String, modifier: Modifier = Modifier) {
+    ElevatedCard(
+        shape = RectangleShape,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        ) {
+            Column {
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                Text(
+                    text = "Subtitle explanation",
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+            Column(horizontalAlignment = Alignment.End) {
+                Text(text = "6:40 PM", style = MaterialTheme.typography.labelSmall)
+                NewMessageCircle()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar() {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text("Order Pilot")
+        }
     )
+}
+
+@Composable
+fun NewMessageCircle() {
+    Box(
+        modifier = Modifier
+            .size(20.dp)
+            .background(color = Color(0xFF25D366), shape = CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "5",
+            color = Color.White,
+            style = MaterialTheme.typography.labelSmall
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CardPreview() {
     OrderpilotTheme {
-        Greeting("Android")
+        Card("Order #1234")
     }
 }
